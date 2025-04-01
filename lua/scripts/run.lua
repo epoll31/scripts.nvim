5bd73dd7f339c57d58bd90f1aac8b5cf1d2f825d
ethan <epollack31@gmail.com>
2025-04-01 16:39:04 -0700

    added love2d test scripts / improved hidden script running

--- lua/scripts/run.lua
+++ lua/scripts/run.lua
@@ -2,22 +2,6 @@ local config = require("scripts.config")
 
 local M = {}
 
----Runs a script
----@param script Script
-function M.run_script(script)
-	-- print(vim.inspect(script))
-	local show_output = script.show_output
-	if show_output == nil then
-		show_output = config.options.default_behaviour.show_output
-	end
-
-	if show_output then
-		M.run_in_window(script.cmd)
-	else
-		M.run_in_cmdline(script.cmd)
-	end
-end
-
 ---@param cmd string
 function M.run_in_window(cmd)
 	if not cmd then
@@ -35,7 +19,31 @@ end
 
 ---@param cmd string
 function M.run_in_cmdline(cmd)
-	vim.fn.system(cmd)
+	-- vim.fn.system(cmd)
+	vim.cmd.new()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, config.options.term_output.height)
+	vim.api.nvim_get_current_buf()
+
+	vim.fn.chansend(vim.bo.channel, { cmd .. "\r\n" })
+	vim.cmd.wincmd("q")
+end
+
+---Runs a script
+---@param script Script
+function M.run_script(script)
+	-- print(vim.inspect(script))
+	local show_output = script.show_output
+	if show_output == nil then
+		show_output = config.options.default_behaviour.show_output
+	end
+
+	if show_output then
+		M.run_in_window(script.cmd)
+	else
+		M.run_in_cmdline(script.cmd)
+	end
 end
 
 return M
